0ba03aa541174a760f867b1e4fdb83c49dda5fe9
Giordon Stark <kratsg@gmail.com>
2022-03-20 16:21:38 -0700

    add test to make sure we can catch problems with missing magic file

--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -2,6 +2,18 @@ from __future__ import annotations
 
 from ctypes.util import find_library
 
+import pylibmagic
+
 
 def test_find_library():
-    assert find_library("magic") is not None
+    lib_path = find_library("magic")
+    assert lib_path is not None
+    assert lib_path.startswith(str(pylibmagic.data))
+
+
+def test_run_magic():
+    import magic
+
+    result = magic.from_file(pylibmagic.data / "__init__.py")
+    assert result
+    assert result == "Python script, ASCII text executable"
