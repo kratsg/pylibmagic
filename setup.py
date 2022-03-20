966224cbc6afbd53e90b3e5a9cb7e173c73ba8b9
Giordon Stark <kratsg@gmail.com>
2022-03-19 22:20:03 -0700

    these changes seem to be getting better

--- setup.py
+++ setup.py
@@ -6,24 +6,10 @@
 
 from __future__ import annotations
 
-from setuptools import setup  # isort:skip
-
-# Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension  # isort:skip
-
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
-
-ext_modules = [
-    Pybind11Extension(
-        "pylibmagic._core",
-        ["src/main.cpp"],
-        cxx_std=11,
-    ),
-]
-
+from skbuild import setup # isort:skip
 
 setup(
-    ext_modules=ext_modules,
+    packages=['pylibmagic'],
+    package_dir={'': 'src'},
+    cmake_install_dir='src/pylibmagic',
 )
