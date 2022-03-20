047632227f179a4e6f9e349a1b630ed6dcc7dfc5
Giordon Stark <kratsg@gmail.com>
2022-03-20 01:29:03 -0700

    get the tests working

--- src/pylibmagic/__init__.py
+++ src/pylibmagic/__init__.py
@@ -7,6 +7,32 @@ pylibmagic: scikit-build project with CMake for compiling libmagic
 
 from __future__ import annotations
 
+import os
+import sys
+
 from ._version import version as __version__
 
-__all__ = ("__version__",)
+if sys.version_info >= (3, 9):
+    from importlib import resources
+else:
+    import importlib_resources as resources
+data = resources.files(__name__)
+
+# structure below matches https://github.com/python/cpython/blob/b3f2d4c8bab52573605c96c809a1e2162eee9d7e/Lib/ctypes/util.py
+keys = []
+if os.name == "nt":
+    keys = ["PATH"]
+elif os.name == "posix" and sys.platform == "darwin":
+    keys = ["DYLD_LIBRARY_PATH"]
+elif sys.platform.startswith("aix"):
+    keys = ["LD_LIBRARY_PATH"]
+elif os.name == "posix":
+    keys = ["LIBRARY_PATH", "LD_LIBRARY_PATH"]
+
+if not keys:
+    raise OSError(f"{os.name} / {sys.platform} not supported yet.")
+
+for key in keys:
+    os.environ[key] = f"{data}{os.pathsep}{os.environ.get(key, '')}"
+
+__all__ = ("__version__", "data")
