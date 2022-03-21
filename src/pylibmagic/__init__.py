44a8f280c82d724b3c4e2e429621f52c58a645dd
Giordon Stark <kratsg@gmail.com>
2022-03-20 23:15:12 -0700

    fix up typing, name should not be optional

--- src/pylibmagic/__init__.py
+++ src/pylibmagic/__init__.py
@@ -53,17 +53,16 @@ if sys.platform == "linux":
 
     def __magic_init__(
         self: ctypes.CDLL,
-        name: str | None,
+        name: str,
         mode: int = ctypes.DEFAULT_MODE,
         handle: int | None = None,
         use_errno: bool = False,
         use_last_error: bool = False,
         winmode: int | None = None,
     ) -> None:
-        if name:
-            path = data / name
-            if path.is_file():
-                name = str(path)
+        path = data / name
+        if path.is_file():
+            name = str(path)
         __cdll_init__(
             self,
             name,
