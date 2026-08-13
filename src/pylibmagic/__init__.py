1f2e38d338e0e603b17e40168e9ca47abe17fa75
Giordon Stark <kratsg@gmail.com>
2026-08-13 13:53:33 -0500

    pre-commit

--- src/pylibmagic/__init__.py
+++ src/pylibmagic/__init__.py
@@ -31,7 +31,9 @@ elif os.name == "posix":
     keys = ["LIBRARY_PATH", "LD_LIBRARY_PATH"]
 
 if not keys:
-    msg = f"{os.name} / {sys.platform} not supported yet."  # pylint: disable=invalid-name
+    msg = (
+        f"{os.name} / {sys.platform} not supported yet."  # pylint: disable=invalid-name
+    )
     raise OSError(msg)
 
 for key in keys:
